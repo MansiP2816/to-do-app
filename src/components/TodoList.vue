0b679203467d311d620712aa1529e1b297077d07
MansiP2816 <mansipurohit802@gmail.com>
2025-09-14 15:46:28 +0530

    Create TodoList.vue

---
+++ src/components/TodoList.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h3>My Todos</h3>
+    <div v-for="todo in todos" :key="todo.id">
+      <todo-item :todo="todo" @updated="emit('todo-updated')" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import TodoItem from "./TodoItem.vue";
+defineProps({ todos: Array });
+</script>
